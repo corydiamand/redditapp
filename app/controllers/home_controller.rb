2aa71c3c672426ea4624cd794878d552d49d3687
Cory <corydiamand@corydiamand-K55N.(none)>
2013-12-13 01:16:58 -0500

    parsing links from reddit homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,17 +8,24 @@ class HomeController < ApplicationController
 		res = Net::HTTP.start(url.host, url.port) {|http|
 		  http.request(req)
 		}
-		#puts res.body
+		
+		#@frontpage = JSON.parse(res.body)
 		@frontpage = JSON.parse(res.body)
-		@frontpagetitles = @frontpage["data"]
-		puts parse_reddit_data(@frontpage)
+		@frontpage2 = []
+
+		@frontpage["data"]["children"].each do |x|
+			@frontpage2.push(x["data"]["permalink"])
+		end
+
+		
+	
 	end
 
 	def search
 	end
 
-	def parse_reddit_data(reddit_data)
-		puts reddit_data
+	def parse_permalinks(reddit_data)
+		
 	end
 
 end
